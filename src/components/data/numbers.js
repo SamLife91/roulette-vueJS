0a29ec6fe47aa7e36b61f44d27043ed91585f406
SamLife91 <sbarguti@quanterall.com>
2018-01-14 04:55:51 +0200

    create outside type bets start doing refactoring

---
+++ src/components/data/numbers.js
@@ -0,0 +1,152 @@
+// sequence
+// 0-32-15-19-4-21-2-25-17-34-6-27-13-36-11-30-8-23-10-5-24-16-33-1-20-14-31-9-22-18-29-7-28-12-35-3-26
+export default [
+  {
+    'value': 0,
+    'color': 'green'
+  },
+  {
+    'value': 32,
+    'color': 'red'
+  },
+  {
+    'value': 15,
+    'color': 'black'
+  },
+  {
+    'value': 19,
+    'color': 'red'
+  },
+  {
+    'value': 4,
+    'color': 'black'
+  },
+  {
+    'value': 21,
+    'color': 'red'
+  },
+  {
+    'value': 2,
+    'color': 'black'
+  },
+  {
+    'value': 25,
+    'color': 'red'
+  },
+  {
+    'value': 17,
+    'color': 'black'
+  },
+  {
+    'value': 34,
+    'color': 'red'
+  },
+  {
+    'value': 6,
+    'color': 'black'
+  },
+  {
+    'value': 27,
+    'color': 'red'
+  },
+  {
+    'value': 13,
+    'color': 'black'
+  },
+  {
+    'value': 36,
+    'color': 'red'
+  },
+  {
+    'value': 11,
+    'color': 'black'
+  },
+  {
+    'value': 30,
+    'color': 'red'
+  },
+  {
+    'value': 8,
+    'color': 'black'
+  },
+  {
+    'value': 23,
+    'color': 'red'
+  },
+  {
+    'value': 10,
+    'color': 'black'
+  },
+  {
+    'value': 5,
+    'color': 'red'
+  },
+  {
+    'value': 24,
+    'color': 'black'
+  },
+  {
+    'value': 16,
+    'color': 'red'
+  },
+  {
+    'value': 33,
+    'color': 'black'
+  },
+  {
+    'value': 1,
+    'color': 'red'
+  },
+  {
+    'value': 20,
+    'color': 'black'
+  },
+  {
+    'value': 14,
+    'color': 'red'
+  },
+  {
+    'value': 31,
+    'color': 'black'
+  },
+  {
+    'value': 9,
+    'color': 'red'
+  },
+  {
+    'value': 22,
+    'color': 'black'
+  },
+  {
+    'value': 18,
+    'color': 'red'
+  },
+  {
+    'value': 29,
+    'color': 'black'
+  },
+  {
+    'value': 7,
+    'color': 'red'
+  },
+  {
+    'value': 28,
+    'color': 'black'
+  },
+  {
+    'value': 12,
+    'color': 'red'
+  },
+  {
+    'value': 35,
+    'color': 'black'
+  },
+  {
+    'value': 3,
+    'color': 'red'
+  },
+  {
+    'value': 26,
+    'color': 'black'
+  }
+]
